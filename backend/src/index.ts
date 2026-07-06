19b7ec9c877dbcd7e394231df891cf9095df8792
Rajath Kumar <77227068+Rajathk6@users.noreply.github.com>
2026-07-06 17:43:38 +0530

    Feature/GitHub actions (#13)
    * fix(github): eslint husky configured
    
    * feat(config): add environment configuration
    
    * fix(github): history inconsistency
    
    * chore(infrastructure): add Redis Docker environment (#6)
    
    * ci: add backend workflow

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,6 @@
-console.log("ReserveX Backend Started");
+import { env } from './config/env.js';
+
+console.log('Application Starting...');
+console.log(env.PORT);
+console.log(env.NODE_ENV);
+console.log(env.REDIS_PORT);
