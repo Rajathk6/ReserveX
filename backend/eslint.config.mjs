90dd3281022e347a787aa502bc58a7cc16b6b0c4
rajath <rajathkumar120@gmail.com>
2026-07-06 12:37:58 +0530

    chore(backend): configure husky and lint-staged

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -4,6 +4,6 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 export default [
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  ...eslintConfigPrettier,
+  eslintConfigPrettier,
   { ignores: ['dist', 'node_modules'] },
 ];
