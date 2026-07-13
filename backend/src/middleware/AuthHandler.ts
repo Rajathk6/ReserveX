372e64f63fd7127ce7139a4fa4416c0335b288bc
rajath <rajathkumar120@gmail.com>
2026-07-13 17:30:20 +0530

    feat(auth): implement RBAC

--- backend/src/middleware/AuthHandler.ts
+++ backend/src/middleware/AuthHandler.ts
@@ -1,10 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import { JwtService } from '../modules/authentication/services/jwt.service.js';
 import { AppError } from '../errors/appErrors.js';
+import { UserRole } from '@prisma/client';
 
 const jwtService = new JwtService();
 
-export async function authHandler(req: Request, _res: Response, next: NextFunction) {
+export function authenticationHandler(req: Request, _res: Response, next: NextFunction) {
   const auth = req.headers.authorization;
   if (!auth || !auth.startsWith('Bearer ')) {
     throw new AppError(401, 'Authentication Required');
@@ -19,4 +20,17 @@ export async function authHandler(req: Request, _res: Response, next: NextFuncti
   };
 
   next();
+}
+
+export function authorizationHandler(...allowedRoles: UserRole[]) {
+  return (req: Request, _res: Response, next: NextFunction) => {
+    if (!req.user) {
+      throw new AppError(401, 'Authentication Required');
+    }
+
+    if (!allowedRoles.includes(req.user.role)) {
+      throw new AppError(403, 'Forbidden, Access Denied');
+    }
+    next();
+  };
 }
