0e19e5d3cae1651fc56ae906858415c2f74c8dca
rajath <rajathkumar120@gmail.com>
2026-07-16 11:09:03 +0530

    refactor(database): seed script

--- backend/prisma/scripts/seed.ts
+++ backend/prisma/scripts/seed.ts
@@ -1,11 +1,9 @@
-import { prisma } from '../src/config/database.js';
-import logger from '../src/config/logger.js';
-import { AuthService } from '../src/modules/authentication/services/auth.service.js';
-import { handlePrismaError } from '../src/utils/prismaError.js';
+import { prisma } from '../../src/config/database.js';
+import logger from '../../src/config/logger.js';
+import { handlePrismaError } from '../../src/utils/prismaError.js';
+import { hashPassword } from '../../src/modules/authentication/utils/password.js';
 
-const authService = new AuthService();
-
-const hash = await authService.hashPassword('admin@123');
+const hash = await hashPassword('admin@123');
 
 async function main() {
   await prisma.user.upsert({
