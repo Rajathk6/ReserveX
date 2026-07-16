f5e9faec0413a42fc9c2b1f9c5912c3ba55f2b0e
rajath <rajathkumar120@gmail.com>
2026-07-16 11:24:20 +0530

    fix(auth): broken package modules

--- backend/src/modules/authentication/services/auth.service.ts
+++ backend/src/modules/authentication/services/auth.service.ts
@@ -1,10 +1,10 @@
 import ms from 'ms';
 
 import { UserRepository } from '../../user/repository/user.respository.js';
-import { LoginDTO, RegisterDTO } from '../validators/auth.validator.js';
+import { LoginDTO, RegisterDTO } from '../types/auth.type.js';
 import { AppError } from '../../../errors/appErrors.js';
 import { generateAccessToken } from '../utils/jwt.js';
-import { RefreshTokenRepository } from '../repositories/refreshToken.repository.js';
+import { RefreshTokenRepository } from '../repositories/refreshToken.repositor.js';
 import env from '../../../config/env.js';
 import { hashPassword, verifyPassword } from '../utils/password.js';
 import { generateRefreshToken, generateRefreshTokenHash } from '../utils/refreshToken.js';
