913235e1b3f294654f9ebfc64b2754bf7e76c33a
rajath <rajathkumar120@gmail.com>
2026-07-17 09:47:55 +0530

    feat(events): Event status handler

--- backend/src/routes/api.routes.ts
+++ backend/src/routes/api.routes.ts
@@ -3,11 +3,12 @@ import { Router } from 'express';
 import coreRouter from '../modules/core/routes/core.routes.js';
 import authRouter from '../modules/authentication/routes/auth.routes.js';
 import eventRouter from '../modules/event/routes/event.route.js';
+import eventstatusRouter from '../modules/event/routes/eventStatus.js';
 
 const route = Router();
 
 route.use('/', coreRouter);
 route.use('/auth', authRouter);
-route.use('/events', eventRouter);
+route.use('/events', eventRouter, eventstatusRouter);
 
 export default route;
